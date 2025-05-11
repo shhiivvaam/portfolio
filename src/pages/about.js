add8e6925b2a1fba3dea8c7c20d785e6b2cfa1ed
astuto-shhiivvaam <shivamkumar@astuto.ai>
2025-05-11 15:53:46 +0530

    updating profile info

--- src/pages/about.js
+++ src/pages/about.js
@@ -66,7 +66,7 @@ const About = () => {
                             </div>
                             <div className='flex flex-col items-end justify-center xl:items-center'>
                                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                                    <AnimatedNumbers value={5} duration={3000} />+
+                                    <AnimatedNumbers value={3} duration={3000} />+
                                 </span>
                                 <h2 className='text-lg font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm '>
                                     Working Projects
